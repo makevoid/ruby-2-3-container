86993c96eb38a6e28f8e402576a4cbbbcb175e53
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2015-12-27 01:21:42 +0000

    be more preice :)

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@
 
 puts "executing frozen string literal test...\nit should fail...\ncomment line 6 to make it work!\n"
 
-str = "this is a frozen string and..."
-str << "this will not be printed"
+str = "this is a frozen string"
+str << "...and this will not be printed"
 
 puts "str: #{str}"
