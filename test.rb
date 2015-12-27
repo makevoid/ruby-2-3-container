7c26d135f9efae3dd21f6ef1645c0d738939aefb
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2015-12-27 01:20:27 +0000

    add explanation of the error

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-puts "executing frozen string literal test...\n"
+puts "executing frozen string literal test...\nit should fail...\ncomment line 6 to make it work!\n"
 
 str = "this is a frozen string and..."
 str << "this will not be printed"
